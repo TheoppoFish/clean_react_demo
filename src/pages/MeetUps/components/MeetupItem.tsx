69f5d1d48168f88cf52233e932bb7d62ae3625c1
zhuolinDeng <zhuolin.deng@outlook.com>
2024-03-16 12:31:18 +0800

    set up vitest

--- src/pages/MeetUps/components/MeetupItem.tsx
+++ src/pages/MeetUps/components/MeetupItem.tsx
@@ -1,20 +1,21 @@
-import React, {FC} from "react";
-import classes from './MeetupItem.module.css';
-import {useNavigate} from "react-router-dom";
-import {meetup} from "@/types";
-import {Card} from "@/components/layout";
+import React, { FC } from "react";
+import { useNavigate } from "react-router-dom";
 
+import { meetup } from "@/types";
+import { Card } from "@/components/layout";
+
+import classes from "./MeetupItem.module.css";
 
 interface MeetupItemProps {
-  item: meetup
+  item: meetup;
 }
-const MeetupItem:FC<MeetupItemProps>=({item}) =>{
+const MeetupItem: FC<MeetupItemProps> = ({ item }) => {
   const navigate = useNavigate();
   const clickHandler = () => navigate(`/meetups/${item.id}`);
 
   return (
     <li className={classes.item}>
-      <Card >
+      <Card>
         <div className={classes.image}>
           <img src={item.image} alt={item.title} />
         </div>
@@ -28,6 +29,6 @@ const MeetupItem:FC<MeetupItemProps>=({item}) =>{
       </Card>
     </li>
   );
-}
+};
 
 export default MeetupItem;
